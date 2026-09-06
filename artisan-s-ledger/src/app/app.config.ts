30784e86d230c2c57bb685e294940f149c212a05
Bryan Tang <89543725+Kaiyan-Da-Man@users.noreply.github.com>
2026-09-06 13:47:15 -0500

    firebase hosting setup

--- artisan-s-ledger/src/app/app.config.ts
+++ artisan-s-ledger/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"artisan-s-ledger-35040","appId":"1:697086090667:web:c782d805c3f11427cd86ee","storageBucket":"artisan-s-ledger-35040.firebasestorage.app","apiKey":"AIzaSyCQjba7b3DKIQ9ubpuH73A8EHd1FcqEn_8","authDomain":"artisan-s-ledger-35040.firebaseapp.com","messagingSenderId":"697086090667","measurementId":"G-FFGW6FGX9Z","projectNumber":"697086090667","version":"2"})), provideFirestore(() => getFirestore())]
 };
